ff080db8e75bf08765a994fd9f205f11c92499cc
Brent <brenteliason@gmail.com>
2018-09-29 09:24:35 -0400

    Added BookList, Book, and BookShelfChanger components, implemented BookList and BookShelfChanger for all books, and Book for one book

---
+++ src/Book.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import BookShelfChanger from './BookShelfChanger'
+
+class Book extends Component {
+	render() {
+      console.log(this.props.title);
+      console.log(this.props.authors);
+      console.log(this.props.width);
+      console.log(this.props.height);
+      console.log(this.props.backgroundImage);
+      return (
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width:this.props.width, height:this.props.height, backgroundImage: this.props.backgroundImage }}></div>
+            <BookShelfChanger/>
+          </div>
+          <div className="book-title">{this.props.title}</div>
+          <div className="book-authors">{this.props.authors}</div>
+        </div>
+      )
+    }
+}
+
+export default Book
---
+++ src/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+	render() {
+      return (
+        <div className="list-books-title">
+       		<h1>MyReads</h1>
+       	</div>
+      )
+    }
+}
+
+export default BookList
